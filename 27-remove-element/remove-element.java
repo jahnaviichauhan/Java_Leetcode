111f7f8df7bc05a8643105f481f608c28453f9f2
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-07-22 21:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (22.96%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i, k=0, temp;
+        int n=nums.length;
+
+        for(i=0; i<n;i++)
+        {
+            if(nums[i]==val)
+            {
+                k++;
+                }
+            }
+        
+        for(i=0; i<n-k;i++)
+        {
+            if(nums[i]==val)
+            {
+               for(int j=n-1; j>=0; j--)
+               {
+                if(nums[j]!=val)
+                {
+                    temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                }
+               }
+            }
+        }
+
+        return (n-k);
+    }
+}
