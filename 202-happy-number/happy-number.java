85d82ca87678f51ef69ac22cb671ba17c2a84194
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-07-31 10:15:34 +0530

    Time: 1 ms (63.02%) | Memory: 40.9 MB (24.43%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+    
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
