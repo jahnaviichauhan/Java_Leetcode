e503da6c8eddf6387087ecb497c91ef92a829069
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-08-09 12:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (60.00%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+         int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i <= rows - 3; i++) {
+            for (int j = 0; j <= cols - 3; j++) {
+                if (isMagicSquare(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isMagicSquare(int[][] grid, int row, int col) {
+        boolean[] seen = new boolean[10]; // To check distinct numbers from 1 to 9
+
+        int sum = grid[row][col] + grid[row][col + 1] + grid[row][col + 2]; // Sum of the first row
+
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < 3; j++) {
+                int num = grid[row + i][col + j];
+                if (num < 1 || num > 9 || seen[num]) {
+                    return false;
+                }
+                seen[num] = true;
+                rowSum += num;
+            }
+            if (rowSum != sum) {
+                return false;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            int colSum = 0;
+            for (int i = 0; i < 3; i++) {
+                colSum += grid[row + i][col + j];
+            }
+            if (colSum != sum) {
+                return false;
+            }
+        }
+
+        // Check diagonals
+        if (grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2] != sum) {
+            return false;
+        }
+        if (grid[row][col + 2] + grid[row + 1][col + 1] + grid[row + 2][col] != sum) {
+            return false;
+        }
+
+        return true;
+    }
+}
