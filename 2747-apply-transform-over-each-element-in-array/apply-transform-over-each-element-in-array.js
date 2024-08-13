e3e78b8fa47cd69dbf2e5328e8b32dcd1b8f6f90
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-08-13 16:20:41 +0530

    Time: 55 ms (36.22%) | Memory: 49.1 MB (10.41%) - LeetSync

---
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;};
