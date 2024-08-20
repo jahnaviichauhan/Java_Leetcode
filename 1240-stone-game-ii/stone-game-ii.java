d7e61f7b6677858b4e2bd5900011d1e59c34b038
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-08-20 16:11:22 +0530

    Time: 8 ms (57.28%) | Memory: 42.6 MB (61.89%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+        int[][] dp = new int[n][n + 1];
+        int[] suffixSum = new int[n];
+        
+        // Calculate the suffix sums
+        suffixSum[n - 1] = piles[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        }
+        
+        // Fill the dp table
+        for (int i = n - 1; i >= 0; i--) {
+            for (int M = 1; M <= n; M++) {
+                if (i + 2 * M >= n) {
+                    dp[i][M] = suffixSum[i];
+                } else {
+                    for (int X = 1; X <= 2 * M; X++) {
+                        dp[i][M] = Math.max(dp[i][M], suffixSum[i] - dp[i + X][Math.max(M, X)]);
+                    }
+                }
+            }
+        }
+        
+        // The result is the maximum number of stones Alice can collect starting from the first pile with M = 1
+        return dp[0][1];
+    }
+}
