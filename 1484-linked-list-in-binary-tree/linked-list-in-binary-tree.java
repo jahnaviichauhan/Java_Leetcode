8b401b7d5530803fe2bbd119776abb649dd98337
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-09-07 21:54:07 +0530

    Time: 1 ms (97.88%) | Memory: 45.2 MB (18.69%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return dfs(root, head) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean dfs(TreeNode node, ListNode head) {
+        if (head == null) {
+            return true;
+        }
+        if (node == null) {
+            return false;
+        }
+        if (node.val != head.val) {
+            return false;
+        }
+        return dfs(node.left, head.next) || dfs(node.right, head.next);
+    }
+
+    // Helper method to build a binary tree from an array (for testing purposes)
+    public static TreeNode buildTree(Integer[] arr) {
+        if (arr.length == 0) return null;
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty() && i < arr.length) {
+            TreeNode current = queue.poll();
+            if (arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    // Helper method to build a linked list from an array (for testing purposes)
+    public static ListNode buildList(int[] arr) {
+        if (arr.length == 0) return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode current = head;
+        for (int i = 1; i < arr.length; i++) {
+            current.next = new ListNode(arr[i]);
+            current = current.next;
+        }
+        return head;
+    }
+}
